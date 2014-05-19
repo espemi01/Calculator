e1fc91ef40ca371375ad6bd0c7091928d1357f8a
klugeh01 <klugeh01@luther.edu>
2014-05-18 20:22:06 -0500

    callbacks

--- main.cpp
+++ main.cpp
@@ -30,26 +30,34 @@ int main(int argc, char *argv[]){
 
   Number_Button *b5  = new Number_Button (10    , 157.5 , 52.5 , 52.5 , 7 , "7" );
   Number_Button *b6  = new Number_Button (72.5  , 157.5 , 52.5 , 52.5 , 8 , "8" );
-  Number_Button *b7  = new Number_Button (135   , 157.5 , 52.5 , 52.5 , 8 , "9" );
-  Op_Button     *b8  = new Op_Button     (197.5 , 157.5 , 52.5 , 52.5 , "-" );
+  Number_Button *b7  = new Number_Button (135   , 157.5 , 52.5 , 52.5 , 9 , "9" );
+  Op_Button     *b8  = new Op_Button     (197.5 , 157.5 , 52.5 , 52.5 ,     "-" );
 
   Number_Button *b9  = new Number_Button (10    , 220 , 52.5 , 52.5 , 4 , "4" );
   Number_Button *b10 = new Number_Button (72.5  , 220 , 52.5 , 52.5 , 5 , "5" );
   Number_Button *b11 = new Number_Button (135   , 220 , 52.5 , 52.5 , 6 , "6" );
-  Op_Button     *b12 = new Op_Button     (197.5 , 220 , 52.5 , 52.5 , "*" );
+  Op_Button     *b12 = new Op_Button     (197.5 , 220 , 52.5 , 52.5 ,     "*" );
 
   Number_Button *b13 = new Number_Button (10    , 282.5 , 52.5 , 52.5 , 1 , "1" );
   Number_Button *b14 = new Number_Button (72.5  , 282.5 , 52.5 , 52.5 , 2 , "2" );
   Number_Button *b15 = new Number_Button (135   , 282.5 , 52.5 , 52.5 , 3 , "3" );
-  Op_Button     *b16 = new Op_Button     (197.5 , 282.5 , 52.5 , 52.5 , "/" );
+  Op_Button     *b16 = new Op_Button     (197.5 , 282.5 , 52.5 , 52.5 ,     "/" );
 
-  Op_Button     *b17 = new Op_Button     (10    , 345 , 52.5 , 52.5 , "C" );
+  Op_Button     *b17 = new Op_Button     (10    , 345 , 52.5 , 52.5 ,     "C" );
   Number_Button *b18 = new Number_Button (72.5  , 345 , 52.5 , 52.5 , 0 , "0" );
-  Op_Button     *b19 = new Op_Button     (135   , 345 , 52.5 , 52.5 , "." );
-  Op_Button     *b20 = new Op_Button     (197.5 , 345 , 52.5 , 52.5 , "E" );
+  Op_Button     *b19 = new Op_Button     (135   , 345 , 52.5 , 52.5 ,     "." );
+  Op_Button     *b20 = new Op_Button     (197.5 , 345 , 52.5 , 52.5 ,     "E" );
 
   b18 -> callback(Number_Button::clickcb);
-  
+  b15 -> callback(Number_Button::clickcb);
+  b14 -> callback(Number_Button::clickcb);
+  b13 -> callback(Number_Button::clickcb);
+  b11 -> callback(Number_Button::clickcb);
+  b10 -> callback(Number_Button::clickcb);
+  b9  -> callback(Number_Button::clickcb);
+  b7  -> callback(Number_Button::clickcb);
+  b6  -> callback(Number_Button::clickcb);
+  b5  -> callback(Number_Button::clickcb);
   // done with widgets
   
   window->end();
